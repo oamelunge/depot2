5f39e0a82abc0de084ad00f90104184607e684dc
oscar <oscar.amelnge@gmai.dom>
2014-05-06 00:28:28 -0400

    ultio test y cpitulo 8

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -46,4 +46,18 @@ class ProductTest < ActiveSupport::TestCase
 
   end
 
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                 product.errors[:title]
+
+  end
+
+
+
 end
